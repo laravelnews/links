09ecc36e6391b778ccdff5a508ca7a43ada118bf
Eric Barnes <eric@ericlbarnes.com>
2015-12-17 22:45:50 -0500

    Making it purty.

--- app/Links.php
+++ app/Links.php
@@ -9,9 +9,14 @@ class Links extends Model
 
     protected $table = 'links';
 
-    protected $fillable = ['category_id', 'user_id', 'title', 'url', 'description', 'published', 'word_count', 'image_url', 'published_at'];
+    protected $fillable = [
+        'category_id', 'user_id', 'title', 'url',
+        'description', 'published', 'published_at'
+    ];
 
-    protected $dates = ['created_at', 'updated_at', 'published_at'];
+    protected $dates = [
+        'created_at', 'updated_at', 'published_at'
+    ];
 
     public function user()
     {
